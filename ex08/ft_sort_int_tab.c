df5d388ea197de786dc7f3488609a6c2c10601dd
Alexandre Dubeau <adubeau@c2r4p5.42quebec.com>
2021-02-17 10:37:58 -0500

    1er commit

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/16 14:36:08 by adubeau           #+#    #+#             */
+/*   Updated: 2021/02/17 10:13:42 by adubeau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int i;
+	int j;
+	int temp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
